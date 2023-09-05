25348f918fdfdafce1baf1f565dbd48b5a147da2
princeemtiaz <soulemtiaz@gmail.com>
2023-09-06 01:00:54 +0600

    for static file

--- coffe_shop/settings.py
+++ coffe_shop/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT= BASE_DIR /'static'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
